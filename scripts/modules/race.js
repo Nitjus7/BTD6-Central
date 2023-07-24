5544380b9abf877e78e9c14f6091e2832e8b2982
Nitjus <110048018+Nitjus7@users.noreply.github.com>
2023-07-23 21:34:18 -0600

    fuuuuuuuuuuuuuck off with events for now

--- scripts/modules/race.js
+++ scripts/modules/race.js
@@ -1,8 +1,15 @@
+import { figureOutEndTime } from "./time";
+import { figureOutStartTime } from "./time";
+
 fetchMeSomeRaceDataAlmightyComputerOverlord = () => {
   fetch("https://data.ninjakiwi.com/btd6/races?")
     .then((response) => response.json())
     .then((data) => {
-      // Use the data
+      const startTime = data.body.start;
+      const startDate = figureOutStartTime(startTime);
+      const endTime = data.body.end;
+      const endDate = figureOutEndTime(endTime);
+      return `The event starts at ${startTime}, and ends at ${endTime}`;
     })
     .catch((error) => {
       // Handle any errors
