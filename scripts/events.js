5acaeaebce542c4a355619d416b8758653a1fb64
Nitjus <110048018+Nitjus7@users.noreply.github.com>
2023-08-19 18:43:03 -0600

    metadata for races now completely finished. up next is implementing leaderboards and other events.

--- scripts/events.js
+++ scripts/events.js
@@ -11,11 +11,46 @@ const specialModsContainer = document.getElementById("specialModsContainer");
 const enabledTowersContainer = document.getElementById(
   "enabledTowersContainer"
 );
-const enabledTowers = document.getElementById("enabledTowers");
+const enabledPrimary = document.getElementById("enabledPrimary");
+const enabledMilitary = document.getElementById("enabledMilitary");
+const enabledMagic = document.getElementById("enabledMagic");
+const enabledSupport = document.getElementById("enabledSupport");
 const enabledHeroes = document.getElementById("enabledHeroes");
 const eventDates = document.getElementById("eventDates");
 const mapContainer = document.getElementById("mapContainer");
 const mapImage = document.getElementById("mapImage");
+
+const primaryTowers = [
+  "DartMonkey",
+  "BoomerangMonkey",
+  "BombShooter",
+  "TackShooter",
+  "IceMonkey",
+  "GlueGunner",
+];
+const militaryTowers = [
+  "SniperMonkey",
+  "MonkeySub",
+  "MonkeyBuccaneer",
+  "MonkeyAce",
+  "HeliPilot",
+  "MortarMonkey",
+  "DartlingGunner",
+];
+const magicTowers = [
+  "WizardMonkey",
+  "SuperMonkey",
+  "NinjaMonkey",
+  "Alchemist",
+  "Druid",
+];
+const supportTowers = [
+  "BananaFarm",
+  "SpikeFactory",
+  "MonkeyVillage",
+  "EngineerMonkey",
+  "BeastHandler",
+];
 
 let startDate;
 let endDate;
@@ -89,8 +124,8 @@ function generateTimestamps(data, parentElem) {
     month: "short",
     day: "numeric",
     year: "numeric",
-    hour: "numeric",
-    minute: "2-digit",
+    /*hour: "numeric",
+    minute: "2-digit",*/
   };
   const startDate = new Date(data["start"]);
   const endDate = new Date(data["end"]);
@@ -130,7 +165,6 @@ function generateRaceMetadata(raceNum, data) {
 // uuuuugggggghhhhhh
 // kill me please
 function displayInfo(metadata) {
-  enabledTowers.innerText = "";
   showMap(metadata);
   document.getElementById(
     "startingCash"
@@ -150,16 +184,29 @@ function displayInfo(metadata) {
   document.getElementById(
     "mode"
   ).innerHTML = `<b>${metadata["difficulty"]}</b><br>${mode}`;
+  determineSpecialMods(metadata);
   for (const tower of metadata["_towers"]) {
-    if (!tower["isHero"]) {
-      if (tower["max"] != 0) parseTowerUpgrades(tower);
-    } else {
+    if (tower["isHero"]) {
       if (tower["tower"] == "ChosenPrimaryHero" && tower["max"] != 0) {
         parseHeroes(tower, true);
-      } else if (tower["max"] != 0) {
+      } else {
         parseHeroes(tower, false);
       }
+    } else {
+      if (tower["max"] != 0) parseTowerUpgrades(tower);
     }
+  }
+  if (enabledPrimary.hasChildNodes()) {
+    prependElement(enabledPrimary, `Primary`);
+  }
+  if (enabledMilitary.hasChildNodes()) {
+    prependElement(enabledMilitary, `Military`);
+  }
+  if (enabledMagic.hasChildNodes()) {
+    prependElement(enabledMagic, `Magic`);
+  }
+  if (enabledSupport.hasChildNodes()) {
+    prependElement(enabledSupport, `Support`);
   }
   document.getElementById("modifiersContainer").style.display = "flex";
 }
@@ -191,18 +238,124 @@ function parseTowerUpgrades(tower) {
     enabledUpgrades = `(${path1Tiers}-${path2Tiers}-${path3Tiers})`;
   }
   const towerInfoDisplay = document.createElement("p");
-  enabledTowersContainer.appendChild(towerInfoDisplay);
   towerInfoDisplay.classList.add("towerInfoDisplay");
   if (tower["max"] > 0) {
     towerInfoDisplay.innerText = `${tower["max"]}x ${name}\n${enabledUpgrades}`;
   } else if (tower["max"] == -1) {
     towerInfoDisplay.innerText = `${name}\n${enabledUpgrades}`;
   }
+
+  if (primaryTowers.includes(tower["tower"])) {
+    enabledPrimary.appendChild(towerInfoDisplay);
+  } else if (militaryTowers.includes(tower["tower"])) {
+    enabledMilitary.appendChild(towerInfoDisplay);
+  } else if (magicTowers.includes(tower["tower"])) {
+    enabledMagic.appendChild(towerInfoDisplay);
+  } else if (supportTowers.includes(tower["tower"])) {
+    enabledSupport.appendChild(towerInfoDisplay);
+  }
 }
 
 // same thing as parseTowers but we don't need to worry about upgrades or max tower count
 // (still sucks)
-function parseHeroes(hero, isSelectable) {}
+function parseHeroes(hero, isSelectable) {
+  if (!isSelectable) {
+    if (hero["max"] != 0) {
+      const heroInfoDisplay = document.createElement("p");
+      heroInfoDisplay.innerHTML = `<b>${hero["tower"]
+        .replace(/([A-Z])/g, " $1")
+        .trim()}</b>`;
+      heroInfoDisplay.classList.add("heroInfoDisplay");
+      enabledHeroes.appendChild(heroInfoDisplay);
+    }
+  } else {
+    const heroInfoDisplay = document.createElement("p");
+    heroInfoDisplay.innerHTML = `<b>Any Hero</b>`;
+    heroInfoDisplay.classList.add("heroInfoDisplay");
+    enabledHeroes.appendChild(heroInfoDisplay);
+  }
+}
+
+function appendElement(parent, title, value) {
+  const element = document.createElement("p");
+  element.innerHTML = `<b>${title}</b><br>${value}`;
+  parent.appendChild(element);
+}
+function prependElement(parent, title) {
+  const element = document.createElement("p");
+  element.innerHTML = `<b>${title}</b>`;
+  parent.prepend(element);
+}
+
+/* 
+if (mentalHealth < adequate) {
+  die("now");
+}
+*/
+function determineSpecialMods(metadata) {
+  if (metadata["maxTowers"] != 9999 && metadata["maxTowers"] != 0) {
+    appendElement(
+      specialModsContainer,
+      `Max Towers`,
+      `${metadata["maxTowers"]}`
+    );
+  }
+  if (metadata["abilityCooldownReductionMultiplier"] !== 1) {
+    appendElement(
+      specialModsContainer,
+      `Ability Cooldowns`,
+      `${metadata["abilityCooldownReductionMultiplier"]}x duration`
+    );
+  }
+  if (metadata["removeableCostMultiplier"] !== 1) {
+    appendElement(
+      specialModsContainer,
+      `Obstacle Removal`,
+      `${metadata["removeableCostMultiplier"]}x cost`
+    );
+  }
+  if (metadata["disableMK"] == true) {
+    appendElement(specialModsContainer, `No Monkey Knowledge`, ``);
+  }
+  if (metadata["disableSelling"] == true) {
+    appendElement(specialModsContainer, `No Selling`, ``);
+  }
+  const bloonMods = metadata["_bloonModifiers"];
+  if (bloonMods["allCamo"] == true) {
+    appendElement(specialModsContainer, `All Camo`, ``);
+  }
+  if (bloonMods["allRegen"] == true) {
+    appendElement(specialModsContainer, `All Regrow`, ``);
+  }
+  if (bloonMods["speedMultiplier"] !== 1) {
+    appendElement(
+      specialModsContainer,
+      `Bloon Speed`,
+      `${bloonMods["speedMultiplier"]}x`
+    );
+  }
+  if (bloonMods["moabSpeedMultiplier"] !== 1) {
+    appendElement(
+      specialModsContainer,
+      `MOAB Speed`,
+      `${bloonMods["moabSpeedMultiplier"]}x`
+    );
+  }
+  if (bloonMods["regrowRateMultiplier"] !== 1) {
+    appendElement(
+      specialModsContainer,
+      `Regrow Rate`,
+      `${bloonMods["regrowRateMultiplier"]}x`
+    );
+  }
+  const hpMods = bloonMods["healthMultipliers"];
+  if (hpMods["bloons"] !== 1) {
+    appendElement(specialModsContainer, `Ceramic HP`, `${hpMods["bloons"]}x`);
+  }
+  if (hpMods["moabs"] !== 1) {
+    appendElement(specialModsContainer, `MOAB HP`, `${hpMods["moabs"]}x`);
+  }
+}
 
 // event listener for race button. this needs to be cleaned up
 raceEventButton.onclick = () => {
@@ -253,7 +406,22 @@ backButton.onclick = () => {
   while (mapContainer.hasChildNodes()) {
     mapContainer.removeChild(mapContainer.firstChild);
   }
-  while (enabledTowersContainer.hasChildNodes()) {
-    enabledTowersContainer.removeChild(enabledTowersContainer.firstChild);
+  while (specialModsContainer.hasChildNodes()) {
+    specialModsContainer.removeChild(specialModsContainer.firstChild);
+  }
+  while (enabledPrimary.hasChildNodes()) {
+    enabledPrimary.removeChild(enabledPrimary.firstChild);
+  }
+  while (enabledMilitary.hasChildNodes()) {
+    enabledMilitary.removeChild(enabledMilitary.firstChild);
+  }
+  while (enabledMagic.hasChildNodes()) {
+    enabledMagic.removeChild(enabledMagic.firstChild);
+  }
+  while (enabledSupport.hasChildNodes()) {
+    enabledSupport.removeChild(enabledSupport.firstChild);
+  }
+  while (enabledHeroes.hasChildNodes()) {
+    enabledHeroes.removeChild(enabledHeroes.firstChild);
   }
 };
